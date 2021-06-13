3640f2165aa0d9c59f2954d47043d4a3d96b723b
Chiến Tạ Đức <taducchiennb01@gmai.com>
2021-06-13 22:06:35 +0700

    khung

---
+++ misa.eshop/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <SiderBar/>
+    <Header/>
+  </div>
+</template>
+
+<script>
+import SiderBar from '../src/views/SiderBar.vue'
+import Header from '../src/views/Header.vue'
+
+
+export default {
+  components: {
+    SiderBar,
+    Header
+  }
+}
+</script>
+
+<style>
+
+/* Reset page */
+*{
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+}
+
+#app {
+  margin: 0px;
+  padding: 0px;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+}
+</style>
